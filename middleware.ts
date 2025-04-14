61d8691b1dd24607721f1a1c73e844091b407ed7
y0hab <thomasedison1697@gmail.com>
2025-04-14 18:01:03 +0300

    _docker

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,9 @@ import jwt from "jsonwebtoken";
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("auth_token")?.value;
 
+  console.log("----token-----");
+  console.log(token);
+
   if (!token) {
     return NextResponse.redirect(new URL("/not-found", request.url));
   }
